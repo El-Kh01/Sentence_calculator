5edd8bfec7d87ea1c1f97ca3d1c2611aa69678fe
El-Kh01 <vasabi3651@gmail.com>
2025-02-20 00:50:58 +0300

    Добавлены новые флажки для учета совокупности преступлений

--- script.js
+++ script.js
@@ -10,23 +10,45 @@ document.getElementById('calc-form').addEventListener('submit', function(event) 
     let isAgreement = document.getElementById('agreement').checked;
     let isJury = document.getElementById('jury').checked;
 
+    // Совокупность преступлений
+    let isLightCrimes = document.getElementById('light-crimes').checked;
+    let isSeriousCrimes = document.getElementById('serious-crimes').checked;
+
     let minSentence = minTerm;
     let maxSentence = maxTerm;
 
+    // Учитываем приготовление
     if (isPrep) {
         maxSentence = Math.floor(maxSentence / 2);
     }
+    // Учитываем покушение
     if (isAttempt) {
         maxSentence = Math.floor(maxSentence * 3 / 4);
     }
+    // Учитываем рецидив
     if (isRecidiv) {
         minSentence = Math.max(minSentence, maxSentence / 3);
     }
+    // Учитываем досудебное соглашение
     if (isAgreement) {
         maxSentence = Math.floor(maxSentence * 2 / 3);
     }
+    // Учитываем вердикт присяжных
     if (isJury) {
         maxSentence = Math.floor(maxSentence * 2 / 3);
+    }
+
+    // Совокупность преступлений
+    if (isLightCrimes) {
+        // Если все преступления легкие или средней тяжести, то применяется поглощение.
+        maxSentence = Math.min(maxSentence, maxSentence / 2);
+    }
+
+    if (isSeriousCrimes) {
+        // Если есть тяжкие преступления, то сложение наказаний
+        maxSentence = Math.floor(maxSentence * 1.5);
+        // Ограничиваем наказание не более чем на половину максимального срока наказания за самое тяжкое преступление
+        maxSentence = Math.min(maxSentence, Math.floor(maxTerm * 1.5));
     }
 
     let minYears = Math.floor(minSentence / 12);
